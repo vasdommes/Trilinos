39e3becfbb31d9e80c18b0e650ad342cdafa8cb7
Joe Frye <jfrye@sandia.gov>
2021-10-11 09:24:02 -0600

    WIP SQUISH

--- cmake/std/PullRequestLinuxCarawayDriver.sh
+++ cmake/std/PullRequestLinuxCarawayDriver.sh
@@ -5,7 +5,5 @@ if [ "${Trilinos_CTEST_DO_ALL_AT_ONCE}" == "" ] ; then
 fi
 
 set -x
-module load cmake/3.19.3
-module load rocm/4.3.0
 
 srun -N1 -p MI100 ${WORKSPACE}/Trilinos/cmake/std/PullRequestLinuxDriver.sh
