1a7b32c9677ef9e6b67ef03678249d033bf00bb5
Joe Frye <jfrye@sandia.gov>
2022-01-10 09:43:54 -0700

    Revert "WIP"
    This reverts commit d3828fa669960d6226c81cfe634f3a9a485c044c.

--- cmake/std/PullRequestLinuxCarawayTestingSettings.cmake
+++ cmake/std/PullRequestLinuxCarawayTestingSettings.cmake
@@ -10,7 +10,7 @@ set (CMAKE_C_COMPILER "mpicc" CACHE STRING "Set by default for caraway PR testin
 set (CMAKE_CXX_COMPILER "mpicxx" CACHE STRING "Set by default for caraway PR testing")
 set (CMAKE_FORTRAN_COMPILER "/home/projects/x86-64/gcc/8.2.0/bin/gfortran" CACHE STRING "Set by default for caraway PR testing")
 set (CMAKE_BUILD_TYPE "RELEASE" CACHE STRING "Set by default for caraway PR testing")
-#set (CMAKE_CXX_FLAGS "--gcc-toolchain=/home/projects/x86-64/gcc/8.2.0" CACHE STRING "Set by default for caraway PR testing")
+set (CMAKE_CXX_FLAGS "--gcc-toolchain=/home/projects/x86-64/gcc/8.2.0" CACHE STRING "Set by default for caraway PR testing")
 set (BUILD_SHARED_LIBS "OFF" CACHE STRING "Set by default for caraway PR testing")
 set (Trilinos_EXTRA_LINK_FLAGS "-L/home/projects/x86-64-rocm/netlib-lapack/3.8.0/lib64 -lgfortran -lm" CACHE STRING "Set by default for caraway PR testing")
 set (Trilinos_ENABLE_TESTS ON CACHE BOOL "Set by default for caraway PR testing")
@@ -55,59 +55,3 @@ set (MPI_BASE_DIR "/home/projects/x86-64-rocm/openmpi/4.0.5/gcc/8.2.0" CACHE STR
 set (MPI_EXEC "mpirun" CACHE STRING "Set by default for caraway PR testing")
 set (MPI_EXEC_NUMPROCS_FLAG "-np" CACHE STRING "Set by default for caraway PR testing")
 set (KokkosCore_UnitTest_CudaTimingBased_MPI_1_DISABLE ON CACHE BOOL "Set by default for caraway PR testing")
-
-#New stuff forthe full build
-set (Trilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES ON CACHE BOOL "Set by default for PR testing")
-set (Trilinos_ENABLE_SEACAS OFF CACHE BOOL "Set by default for caraway PR testing")
-set (Trilinos_ENABLE_STK OFF CACHE BOOL "Set by default for caraway PR testing")
-set (Trilinos_ENABLE_ShyLU OFF CACHE BOOL "Set by default for caraway PR testing")
-set (Trilinos_ENABLE_ShyLU_Node OFF CACHE BOOL "Set by default for caraway PR testing")
-
-
-
-
-
-set (TPL_ENABLE_LAPACK ON CACHE BOOL "Set by default for PR testing")
-set (TPL_ENABLE_Boost ON CACHE BOOL "Set by default for PR testing")
-set (TPL_ENABLE_BoostLib ON CACHE BOOL "Set by default for PR testing")
-set (TPL_ENABLE_ParMETIS ON CACHE BOOL "Set by default for PR testing")
-set (TPL_ENABLE_Zlib ON CACHE BOOL "Set by default for PR testing")
-set (TPL_ENABLE_HDF5 ON CACHE BOOL "Set by default for PR testing")
-set (TPL_ENABLE_Netcdf ON CACHE BOOL "Set by default for PR testing")
-set (Trilinos_TRACE_ADD_TEST ON CACHE BOOL "Set by default for PR testing")
-
-
-#trouble building these 2 on caraway
-set (TPL_ENABLE_SuperLU OFF CACHE BOOL "Set by default for PR testing")
-set (TPL_ENABLE_Scotch OFF CACHE BOOL "Set by default for PR testing")
-#set (TPL_ENABLE_Matio OFF CACHE BOOL "Set by default for PR testing")
-#set (TPL_ENABLE_X11 OFF CACHE BOOL "Set by default for PR testing")
-
-
-SET(Trilinos_EXTRA_LINK_FLAGS "-lgomp -lgfortran -lldl -ldl" CACHE STRING "Set by default for PR testing")
-
-SET(Trilinos_ENABLE_PyTrilinos OFF CACHE BOOL "Set by default for PR testing")
-
-SET(Boost_INCLUDE_DIRS "$ENV{BOOST_INC_DIR}" CACHE PATH "Set by default for PR testing")
-SET(Boost_LIBRARY_DIRS "$ENV{BOOST_LIB_DIR}" CACHE PATH "Set by default for PR testing")
-
-SET(BoostLib_INCLUDE_DIRS "$ENV{BOOST_INC_DIR}" CACHE PATH "Set by default for PR testing")
-SET(BoostLib_LIBRARY_DIRS "$ENV{BOOST_LIB_DIR}" CACHE PATH "Set by default for PR testing")
-
-SET(ParMETIS_INCLUDE_DIRS "$ENV{PARMETIS_INC_DIR}" CACHE PATH "Set by default for PR testing")
-SET(ParMETIS_LIBRARY_DIRS "$ENV{PARMETIS_LIB_DIR}" CACHE PATH "Set by default for PR testing")
-
-SET(Zlib_INCLUDE_DIRS "$ENV{ZLIB_INC_DIR}" CACHE PATH "Set by default for PR testing")
-SET(Zlib_LIBRARY_DIRS "$ENV{ZLIB_LIB_DIR}" CACHE PATH "Set by default for PR testing")
-
-SET(HDF5_INCLUDE_DIRS "$ENV{HDF5_INC_DIR}" CACHE PATH "Set by default for PR testing")
-SET(HDF5_LIBRARY_DIRS "$ENV{HDF5_LIB_DIR}" CACHE PATH "Set by default for PR testing")
-
-SET(Netcdf_INCLUDE_DIRS "$ENV{NETCDF_INC_DIR}" CACHE PATH "Set by default for PR testing")
-SET(Netcdf_LIBRARY_DIRS "$ENV{NETCDF_LIB_DIR}" CACHE PATH "Set by default for PR testing")
-
-#SET(SuperLU_INCLUDE_DIRS "$ENV{SUPERLU_INC_DIR}" CACHE PATH "Set by default for PR testing")
-#SET(SuperLU_LIBRARY_DIRS "$ENV{SUPERLU_LIB_DIR}" CACHE PATH "Set by default for PR testing")
-
-#set (TPL_Scotch_INCLUDE_DIRS "$ENV{SCOTCH_INC_DIR}" CACHE PATH "Set by default for PR testing")
-#set (Scotch_LIBRARY_DIRS "$ENV{SCOTCH_LIB_DIR}" CACHE PATH "Set by default for PR testing")
