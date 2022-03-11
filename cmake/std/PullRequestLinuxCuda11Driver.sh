e440285076eef7437e1166fbd31f84afb00a6242
Joe Frye <jfrye@sandia.gov>
2022-03-11 10:10:29 -0700

    infrastructure for new trilinos nightly builds

--- cmake/std/PullRequestLinuxCuda11Driver.sh
+++ cmake/std/PullRequestLinuxCuda11Driver.sh
@@ -14,9 +14,8 @@ fi
 set -x
 
 #TODO: review appropriate job size
-#cuda 10.1.105 will be built with rhel7G
 
-bsub -x -q dev -Is -n 20 -J ${JOB_NAME} -W ${BSUB_CTEST_TIME_LIMIT} ${WORKSPACE}/Trilinos/cmake/std/PullRequestLinuxDriver.sh
+bsub -x -q dev -m "weaver9" -Is -n 20 -J ${JOB_NAME} -W ${BSUB_CTEST_TIME_LIMIT} ${WORKSPACE}/Trilinos/cmake/std/PullRequestLinuxDriver.sh
 
 #if [[ "${JOB_NAME}" == "trilinos-folder/Trilinos_pullrequest_cuda_10.1.105"* ]] ; then
 #  bsub -x -Is -q rhel7G -n 16 -J ${JOB_NAME} -W ${BSUB_CTEST_TIME_LIMIT} ${WORKSPACE}/Trilinos/cmake/std/PullRequestLinuxDriver.sh
